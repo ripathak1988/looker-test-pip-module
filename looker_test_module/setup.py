1e5793b30a266e508fb87991763d952ec8a7adca
Rishi Pathak <ripathak@expedia.com>
2018-03-23 10:36:17 +0530

    Updated version

--- looker_test_module/setup.py
+++ looker_test_module/setup.py
@@ -2,9 +2,9 @@
 from setuptools import setup
 
 setup(name='looker_test_module',
-      version='0.2',
+      version='0.4',
       description='looker_test_module',
-      url='https://github.com/ripathak1988/looker-test-pip-package',
+      url='https://github.com/ripathak1988/looker-test-pip-module',
       author='Rishi Pathak',
       author_email='ripathak@expedia.com',
       license='',
